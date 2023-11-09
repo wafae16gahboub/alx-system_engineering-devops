73d081e26e4daeefd80128a9ce612a58114c4c44
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-11-09 14:32:33 -0800

    task advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" task 1"""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    base_url = 'https://www.reddit.com'
-    headers = {'user-agent': 'fake_user_agent'}
-
-    dict_response = requests.get(
-      base_url + "/r/" + subreddit + "/hot.json",
-      headers=headers,
-      allow_redirects=False).json()
-
-    list_posts = dict_response.get('data', {}).get("children", [])
-    if not list_posts:
+    """Print the title"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
-    for title in list_posts[:10]:
-        print(title.get('data').get('title'))
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
